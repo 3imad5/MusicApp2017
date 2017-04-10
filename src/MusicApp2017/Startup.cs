2264c1126bdac400b3024f5b8e4c06072d88eb32
Emad Khateeb <e.n.khateeb@hotmail.com>
2017-04-09 20:35:31 -0400

    Includes auth.

--- src/MusicApp2017/Startup.cs
+++ src/MusicApp2017/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MusicApp2017.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace MusicApp2017
 {
@@ -32,7 +33,19 @@ namespace MusicApp2017
             // Add framework services.
             services.AddDbContext<MusicDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                        services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<MusicDbContext>()
+                .AddDefaultTokenProviders();
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            });
             services.AddMvc();
         }
 
@@ -52,7 +65,7 @@ namespace MusicApp2017
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
+            app.UseIdentity();
 
             app.UseMvc(routes =>
             {
